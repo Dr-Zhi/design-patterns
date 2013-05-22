e53b8059e3aac6bd175f85a1e63581d7db8a1405
Dr-Zhi <yl.zhi@hotmail.com>
2013-05-22 10:49:57 +0800

    initialize project

---
+++ design-patterns/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  design-patterns
+//
+//  Created by Yanling Zhi on 5/22/13.
+//  Copyright (c) 2013 Dr. Zhi Inc. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+
+    return 0;
+}
+
