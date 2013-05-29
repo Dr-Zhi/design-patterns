b7e8e80ea711edc88ed443ce5efd5a4135b1d808
Dr-Zhi <yl.zhi@hotmail.com>
2013-05-29 20:15:21 +0800

    updates singleton implementation

--- design-patterns/Singleton.h
+++ design-patterns/Singleton.h
@@ -9,14 +9,22 @@
 #ifndef __design_patterns__Singleton__
 #define __design_patterns__Singleton__
 
-#include <iostream>
-
+/** Meyers implementation. 
+ * Q1: is it thread-safe? In C++11, it is, according to standard:
+ * If control enters the declaration concurrently while the variable
+ * is being initialized, the concurrent execution shall wait for 
+ * completion of the initialization.
+ *
+ * References:
+ * [1] http://stackoverflow.com/questions/1661529/is-meyers-implementation-of-singleton-pattern-thread-safe
+ */
 class Singleton {
 public:
-    static Singleton * instance() {
+    inline static Singleton * instance() {
         static Singleton s;
         return &s;
     }
+    
 private:
     Singleton() {}
     Singleton (const Singleton &);
